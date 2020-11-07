afedf67550af444fa47ed82187a441a8a7dcfbd6
Rain999 <1163799833@qq.comgit>
2020-11-07 08:22:47 +0800

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+
+git remote add origin git@gitee.com:rain_999/test.git
---
+++ src/sum/Sum.java
@@ -0,0 +1,13 @@
+package sum;
+
+public class Sum {
+
+    int sun(int i,int j){
+        return i+j;
+    }
+
+
+    public static void main(){
+
+    }
+}
